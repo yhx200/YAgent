38ec97765e0ad2db185f7ef44d1256e0df051239
Droping <164728908+yhx200@users.noreply.github.com>
2026-09-11 16:02:44 +0800

    Create InstallationService.java

---
+++ apps/yagent-platform/src/main/java/com/yagent/platform/service/installation/InstallationService.java
@@ -0,0 +1,10 @@
+package com.yagent.platform.service.installation;
+
+import com.yagent.platform.dto.installation.InstallationResolveRequest;
+import com.yagent.platform.dto.installation.InstallationResolveResponse;
+
+public interface InstallationService {
+
+    InstallationResolveResponse resolve(InstallationResolveRequest request);
+
+}
