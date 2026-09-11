dab99c5524d56a6db701b64697595a380d4a5adb
Droping <164728908+yhx200@users.noreply.github.com>
2026-09-11 16:00:42 +0800

    Create YagentPlatformApplication.java

---
+++ apps/yagent-platform/src/main/java/com/yagent/platform/YagentPlatformApplication.java
@@ -0,0 +1,15 @@
+package com.yagent.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YagentPlatformApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(YagentPlatformApplication.class, args);
+
+    }
+
+}
