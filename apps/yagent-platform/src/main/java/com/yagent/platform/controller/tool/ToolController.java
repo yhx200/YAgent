717c81d243b7e5638b760e0405701d703153ed8a
Droping <164728908+yhx200@users.noreply.github.com>
2026-09-11 19:12:54 +0800

    Update and rename ToolInternalController.java to ToolController.java

--- apps/yagent-platform/src/main/java/com/yagent/platform/controller/tool/ToolController.java
+++ apps/yagent-platform/src/main/java/com/yagent/platform/controller/tool/ToolController.java
@@ -3,14 +3,11 @@ package com.yagent.platform.controller.tool;
 import com.yagent.platform.dto.tool.ToolVersionResponse;
 import com.yagent.platform.service.tool.ToolVersionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/inner/v1/tools")
-public class ToolInternalController {
+public class ToolController {
 
     @Autowired
     private ToolVersionService toolVersionService;
