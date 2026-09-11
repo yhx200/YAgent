8be8d3302f1bef078e5e0480caa7d371fb360cdf
Droping <164728908+yhx200@users.noreply.github.com>
2026-09-11 19:11:26 +0800

    Update InstallationInternalController.java

--- apps/yagent-platform/src/main/java/com/yagent/platform/controller/installations/InstallationInternalController.java
+++ apps/yagent-platform/src/main/java/com/yagent/platform/controller/installations/InstallationInternalController.java
@@ -4,14 +4,11 @@ import com.yagent.platform.dto.installation.InstallationResolveRequest;
 import com.yagent.platform.dto.installation.InstallationResolveResponse;
 import com.yagent.platform.service.installation.InstallationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/inner/v1/installations")
-public class InstallationInternalController {
+public class InstallationController {
 
     @Autowired
     private InstallationService installationService;
