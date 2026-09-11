2b1cd6ed9f624fb84d715abd6d7f351e936e92b1
Droping <164728908+yhx200@users.noreply.github.com>
2026-09-11 15:49:46 +0800

    Add ErrorResponse class for error handling

---
+++ apps/yagent-platform/src/main/java/com/yagent/platform/dto/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.yagent.platform.dto.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String code;
+
+    private String message;
+}
