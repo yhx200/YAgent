c956d5c0fe0dd86789d937dafb6070352581461b
Droping <164728908+yhx200@users.noreply.github.com>
2026-09-11 19:12:27 +0800

    Update PermissionController.java

--- apps/yagent-platform/src/main/java/com/yagent/platform/controller/permission/PermissionController.java
+++ apps/yagent-platform/src/main/java/com/yagent/platform/controller/permission/PermissionController.java
@@ -4,10 +4,7 @@ import com.yagent.platform.dto.permission.PermissionEvaluateRequest;
 import com.yagent.platform.dto.permission.PermissionEvaluateResponse;
 import com.yagent.platform.service.permission.PermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/inner/v1/permissions")
