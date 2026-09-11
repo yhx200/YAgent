cbc512c616c73d6e31fab5be2c16d0ff890e7eaf
Droping <164728908+yhx200@users.noreply.github.com>
2026-09-11 16:01:49 +0800

    Create CapabilityService.java

---
+++ apps/yagent-platform/src/main/java/com/yagent/platform/service/capability/CapabilityService.java
@@ -0,0 +1,10 @@
+package com.yagent.platform.service.capability;
+
+import com.yagent.platform.dto.capability.CapabilitySearchRequest;
+import com.yagent.platform.dto.capability.CapabilitySearchResponse;
+
+public interface CapabilityService {
+
+     CapabilitySearchResponse search(CapabilitySearchRequest request);
+
+}
