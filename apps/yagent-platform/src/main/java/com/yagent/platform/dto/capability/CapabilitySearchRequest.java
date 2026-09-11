7f82abb021de37fe4e885a96b6d33055e9528b17
Droping <164728908+yhx200@users.noreply.github.com>
2026-09-11 15:48:41 +0800

    Create CapabilitySearchRequest.java

---
+++ apps/yagent-platform/src/main/java/com/yagent/platform/dto/capability/CapabilitySearchRequest.java
@@ -0,0 +1,14 @@
+package com.yagent.platform.dto.capability;
+
+import lombok.Data;
+
+@Data
+public class CapabilitySearchRequest {
+
+    private Long tenantId;
+
+    private String query;
+
+    private Integer limit = 5;
+
+}
