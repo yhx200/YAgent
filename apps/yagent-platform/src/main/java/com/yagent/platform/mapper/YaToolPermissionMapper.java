ff12a60216ad020157a925e674e3a5dab0896d89
Droping <164728908+yhx200@users.noreply.github.com>
2026-09-12 13:17:43 +0800

    Create YaToolPermissionMapper.java

---
+++ apps/yagent-platform/src/main/java/com/yagent/platform/mapper/YaToolPermissionMapper.java
@@ -0,0 +1,8 @@
+package com.yagent.platform.mapper;
+import com.yagent.platform.domain.YaToolPermission;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
+@Mapper public interface YaToolPermissionMapper {
+    List<YaToolPermission> selectByToolVersion(@Param("toolId") String toolId,@Param("version") String version);
+}
