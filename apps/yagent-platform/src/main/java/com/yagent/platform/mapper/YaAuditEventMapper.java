5c9fe8f9bc09f1871a751a9155d9719979194be2
Droping <164728908+yhx200@users.noreply.github.com>
2026-09-12 13:16:00 +0800

    Create YaAuditEventMapper.java

---
+++ apps/yagent-platform/src/main/java/com/yagent/platform/mapper/YaAuditEventMapper.java
@@ -0,0 +1,8 @@
+package com.yagent.platform.mapper;
+import com.yagent.platform.domain.YaAuditEvent;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
+@Mapper public interface YaAuditEventMapper {
+    int insert(YaAuditEvent event);
+}
