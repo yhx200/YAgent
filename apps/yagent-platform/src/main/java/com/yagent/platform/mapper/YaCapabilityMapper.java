916608da2bf2a630c89fc0bc5bdc71a4b22a7029
Droping <164728908+yhx200@users.noreply.github.com>
2026-09-12 13:16:18 +0800

    Create YaCapabilityMapper.java

---
+++ apps/yagent-platform/src/main/java/com/yagent/platform/mapper/YaCapabilityMapper.java
@@ -0,0 +1,8 @@
+package com.yagent.platform.mapper;
+import com.yagent.platform.domain.YaCapability;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import java.util.List;
+@Mapper public interface YaCapabilityMapper {
+    List<YaCapability> selectActive();
+}
